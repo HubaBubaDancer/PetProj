e9ed3503afbf436e8448f7bb07e9140c1fa86cf7
hubbub <irobot.exe@gmail.com>
2024-03-02 14:21:35 +0200

    mogrations

---
+++ PetProject/Migrations/20240302122056_main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class main : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "CarId",
+                table: "Contracts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Contracts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Contracts");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CarId",
+                table: "Contracts",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContractIds = table.Column<List<Guid>>(type: "uuid[]", nullable: true),
+                    Info = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.id);
+                });
+        }
+    }
+}
